65ea72780f09b02777f774b563c0e906e11f440a
abidkhan484 <abidkhan484@gmail.com>
2018-08-04 12:28:56 +0600

    just made the temperature toggler

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-var body = document.getElementById("temp");
+var areaInfo = document.getElementById("areaInfo");
+var temperature = document.getElementById("temp");
+var weatherTitle = document.getElementById("weather-title");
 var weatherIcon = document.getElementById("weather-icon");
 
 // call the location and send the position
@@ -23,37 +25,53 @@ function showPosition(position) {
 // perform ajax call
 function httpGetAsync(theUrl, writeDocument)
 {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            writeDocument(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous
-    xmlHttp.send(null);
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function() {
+      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+          writeDocument(xmlHttp.responseText);
+  }
+  xmlHttp.open("GET", theUrl, true); // true for asynchronous
+  xmlHttp.send(null);
 }
+
+
+function tempInnerHtml(temp, unit) {
+  return temp + ' °<span id="toggle-temp">'+ unit +'</span>';
+}
+
+function celsiusToFar(temp) {
+  return Math.round(temp * 1.8 + 32);
+}
+
+function fahrenheitToCel(temp) {
+  return Math.round((temp-32)*(5/9));
+}
+
 
 // finally html document is written
 function writeDocument(response) {
   var res = JSON.parse(response);
   var areaName = res.name;
   var country = res.sys.country;
-  var weatherTitle = res.weather[0].main;
   var temp = res.main.temp;
-  var tempHtml = '<p>' + temp + ' <span onclick=tempChange() id="toggle-temp">°C</span>' + '</p>';
   var iconURL = res.weather[0].icon;
-  var finalHtml = '';
-  finalHtml += '<p>' + areaName + ', ' + country + '</p>';
-  finalHtml += tempHtml;
-  finalHtml += '<p>' + weatherTitle + '</p>';
-  body.innerHTML = finalHtml;
+
+  areaInfo.innerHTML = areaName + ', ' + country;
+  temperature.innerHTML = tempInnerHtml(temp, "C");
+  temperature.onclick = function() {
+    var unit = document.getElementById("toggle-temp");
+    if (unit.innerHTML=="C"){
+      temp = celsiusToFar(temp);
+      temperature.innerHTML = tempInnerHtml(temp, "F");
+    }
+    else{
+      temp = fahrenheitToCel(temp);
+      temperature.innerHTML = tempInnerHtml(temp, "C");
+    }
+  };
+  weatherTitle.innerHTML = res.weather[0].main;
   weatherIcon.innerHTML = '<img src=\"'+iconURL+'\">';
 }
-
-function tempChange() {
-
-}
-
-
 
 
 // calling the main function
